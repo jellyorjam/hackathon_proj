4f13622f562e0416bd5b01ea80646226d5836551
Emily Freeman <freeman.ecf@gmail.com>
2022-05-02 13:38:26 -0400

    covid api working

--- hackathon_proj/src/components/CovidData.js
+++ hackathon_proj/src/components/CovidData.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchLatAndLong, fetchFips } from '../reducers/locationSlice';
+import { fetchCovidData } from '../reducers/covidSlice';
 
 const CovidData = () => {
   const dispatch = useDispatch();
   const zipcode = useSelector(state => state.location.zipcode);
   const latitude = useSelector(state => state.location.latitude);
   const longitude = useSelector(state => state.location.longitude);
+  const fips = useSelector(state => state.location.fips);
 
   useEffect(() => {
     if (zipcode) {
@@ -25,6 +27,11 @@ const CovidData = () => {
     }
   }, [latitude, longitude, dispatch])
 
+  useEffect(() => {
+    if (fips) {
+      dispatch(fetchCovidData(fips))
+    }
+  }, [fips, dispatch])
   
   
   return (
