e3ed3089e0a332a83295eb4af6527fff6fd5f905
Emily Freeman <freeman.ecf@gmail.com>
2022-05-02 13:14:13 -0400

    got fips working

--- hackathon_proj/src/components/CovidData.js
+++ hackathon_proj/src/components/CovidData.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { fetchLatAndLong } from '../reducers/locationSlice';
+import { fetchLatAndLong, fetchFips } from '../reducers/locationSlice';
 
 const CovidData = () => {
-
-  const zipcode = useSelector(state => state.location[0])
+  const dispatch = useDispatch();
+  const zipcode = useSelector(state => state.location.zipcode);
+  const latitude = useSelector(state => state.location.latitude);
+  const longitude = useSelector(state => state.location.longitude);
 
   useEffect(() => {
     if (zipcode) {
-      console.log('use effect')
+      dispatch(fetchLatAndLong(zipcode));
     }
-  }, [zipcode])
+  }, [zipcode, dispatch]);
 
+  useEffect(() => {
+    if (latitude && longitude) {
+      const latAndLong = {
+        lat: latitude,
+        long: longitude
+      }
+      dispatch(fetchFips(latAndLong))
+    }
+  }, [latitude, longitude, dispatch])
+
+  
   
   return (
     <div>Hello</div>
--- hackathon_proj/src/reducers/locationSlice.js
+++ hackathon_proj/src/reducers/locationSlice.js
@@ -3,7 +3,9 @@ import { useSelector } from 'react-redux';
 import axios from 'axios'
 
 const locationUrl = 'https://thezipcodes.com/api/v1/search?zipCode=';
-const locationApiKey = '4b19cd5cd687808ad130966e8ff781ae'
+const locationApiKey = '4b19cd5cd687808ad130966e8ff781ae';
+
+const getCountyInfoUrl = 'https://geo.fcc.gov/api/census/block/find?';
 
 
 const initialState = [];
@@ -11,6 +13,16 @@ const initialState = [];
 export const fetchLatAndLong = createAsyncThunk('location/fetchLatAndLong', async (zipcode) => {
   try {
     const response = await axios.get(locationUrl + zipcode + '&countryCode=US&apiKey=' + locationApiKey)
+    return response.data
+  }
+  catch (err) {
+    return err
+  }
+});
+
+export const fetchFips = createAsyncThunk('location/fetchFips', async (latAndLong) => {
+  try {
+    const response = await axios.get(getCountyInfoUrl + 'latitude=' + latAndLong.lat + '&longitude=' + latAndLong.long + '&censusYear=2020&format=json')
     return response.data
   }
   catch (err) {
@@ -28,7 +40,17 @@ const locationSlice = createSlice ({
   },
   extraReducers: (builder) => {
     builder.addCase(fetchLatAndLong.fulfilled, (state, action) => {
-      state.push(action.payload)
+      return {
+        ...state,
+        latitude: action.payload.location[0].latitude,
+        longitude: action.payload.location[0].longitude
+      }
+    })
+    .addCase(fetchFips.fulfilled, (state, action) => {
+      return {
+        ...state,
+        fips: action.payload.County.FIPS
+      }
     })
   }
 })
