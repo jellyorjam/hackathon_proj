83859dd5e2e9ba2b65f9c30455bb9bd565d6b868
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-05-02 21:08:29 -0500

    initial pollen ul

--- hackathon_proj/src/components/PollenData.js
+++ hackathon_proj/src/components/PollenData.js
@@ -1,6 +1,8 @@
+import _ from "lodash";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPollenData } from "../reducers/pollenSlice";
+
 
 const PollenData = () => {
   const dispatch = useDispatch();
@@ -13,10 +15,52 @@ const PollenData = () => {
     }
   }, [latitude, longitude, dispatch])
 
-  console.log(useSelector(state => state.pollen))
+  const pollenData = useSelector(state => state.pollen)
+  let plants = [];
+
+  const renderPollen = () => {
+    console.log(!_.isEmpty(plants))
+    if(!_.isEmpty(plants)) {
+      return plants.map(p => {
+        switch(p.display_name){
+          case 'Grass':
+            return (
+              <div className='col-4'>
+                <img width='40' src='https://static.thenounproject.com/png/1903-200.png' alt='grass' />
+                <p>{p.display_name}</p>
+              </div>
+            )
+          case 'Weed':
+            return (
+              <div className='col-4'>
+                <img width='40' src='https://cdn-icons-png.flaticon.com/512/25/25207.png' alt='weed' />
+                <p>{p.display_name}</p>
+              </div>
+            )
+          case 'Tree':
+            return (
+              <div className='col-4'>
+                <img width='40' src='https://www.freeiconspng.com/thumbs/tree-icon/tree-icon-png-tree-icon-bw-1.jpg' alt='tree' />
+                <p>{p.display_name}</p>
+              </div>
+            )
+          default:
+            return <p>no pollen</p>
+        }
+      })
+    }
+  }
   
+  if(pollenData[0]){
+    plants.push(pollenData[0].data[0].types.grass)
+    plants.push(pollenData[0].data[0].types.weed)
+    plants.push(pollenData[0].data[0].types.tree)
+  }
+
   return (
-    <div>pollen</div>
+    <div className='row'>
+      {renderPollen()}
+    </div>
   )
 };
 
--- hackathon_proj/src/reducers/pollenSlice.js
+++ hackathon_proj/src/reducers/pollenSlice.js
@@ -4,12 +4,12 @@ import axios from 'axios';
 const initialState = [];
 
 const pollenUrl = 'https://api.breezometer.com/pollen/v2/forecast/daily?lat=';
-const endUrl = '&days=3&key='
+const endUrl = '&days=1&key='
 const pollenApiKey = '6ed80b00f59c48c1aa34e239f05265e2';
 
 export const fetchPollenData = createAsyncThunk('pollen/fetchPollenData', async ({latitude, longitude}) => {
   try {
-    const response = await axios.get(pollenUrl + latitude + '&lon=' + longitude + endUrl + pollenApiKey);
+    const response = await axios.get(pollenUrl + latitude + '&lon=' + longitude + endUrl + pollenApiKey + '&features=types_information,plants_information');
     return response.data;
   }
   catch (err) {
