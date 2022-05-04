ad9f41f883bb735d6b1dabdc3af4e2003c891587
Emily Freeman <freeman.ecf@gmail.com>
2022-05-04 13:42:47 -0400

    add some covid UI

--- hackathon_proj/src/components/CovidData.js
+++ hackathon_proj/src/components/CovidData.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchLatAndLong, fetchFips } from '../reducers/locationSlice';
 import { fetchCovidData, setCovidData, totalCases } from '../reducers/covidSlice';
+import { PieChart } from 'reaviz';
 
 const CovidData = () => {
 
@@ -46,14 +47,55 @@ const CovidData = () => {
     }
   }, [covidState.vaxCompleted, covidState.population, covidState.weeklyNewCasesPer100k, dispatch]);
 
+
+  const renderCovidHeader = () => {
+    if (covidState.readyToRender) {
+      return (
+        <div>
+          <h2 className='covid-header'>{'Covid Stats in ' + locationState.city + ', ' + locationState.state +  ':'}</h2>
+        </div>
+      )
+    }
+  }
+
+  const renderCovidData = () => {
+    if (covidState.readyToRender) {
+      return (
+        <div className='covid-row'>
+          <div className='col-4'>
+          <i className="fa-solid fa-virus-covid fa-6x"></i>
+            <div className='titles'>Total New Cases In the Past 7 Days</div>
+            <div className='nums'>{covidState.totalCases}</div> 
+          </div>
+          <div className='col-4'>
+            <div className='titles'>Test Positivity Ratio</div>
+            <div className='nums'>{covidState.testPositivityRatio}%</div>
+            <div className='titles'>CDC Transmission Level</div>
+            <div className='nums'>{covidState.cdcTransmissionLevel}</div>
+          </div>
+          <div className='col-2'>
+             <PieChart 
+                data={[
+                        { key: '% of Population Vaccinated', data: covidState.vaxCompleted},
+                        { key: '% of Population Not Vaccinated', data: 100 - covidState.vaxCompleted}
+                       ]}
+              />
+          </div>
+          <div className='col-2'> <PieChart 
+                data={[
+                        { key: '% of Population Boosted', data: covidState.vaxWithBooster},
+                        { key: '% of Population Not Boosted', data: 100 - covidState.vaxWithBooster}
+                       ]}
+              /></div>
+        </div>
+      )
+    }
+  }
   
   return (
-    <div>
-        <div>{covidState.totalCases}</div>
-        <div>{covidState.testPositivityRatio}</div>
-        <div>{covidState.vaxCompleted}</div>
-        <div>{covidState.vaxWithBooster}</div>
-        <div>{covidState.cdcTransmissionLevel}</div>
+    <div className='container align-content-center row covid-div'>
+      {renderCovidHeader()}
+      {renderCovidData()}
     </div>
   
   )
